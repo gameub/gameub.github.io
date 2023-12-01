8c6a0061ace0c945b83878cf6a6b1b9a1009059b
gameub <142449811+gameub@users.noreply.github.com>
2023-11-30 21:26:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,12 @@ var splashes=[
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.",
     "Minceraft.",
-    "Don't click this.\nBecause it does nothing.",
+    "Don't click this.Because it does nothing.",
     "Check the changelog sometimes.",
     "geometry dash 🤓",
     "New Tab",
     "I know you have missing assignments.",
     "For when you're bored in class.",
-    "Quite the site.",
     "English grade done by ChatGPT.",
     "Lawsuits sold seperately.",
     "c.ai coming soon!",
@@ -23,6 +22,15 @@ var splashes=[
     "2.3 in 6 months!",
     "Winter update!",
     "Will work for vbucks.",
+    "Best on the market.",
+    "Chromebooks here are worth $20.",
+    "JERRY!!",
+    "Better play with fullscreen mode",
+    "The teachers dont know... Or do they? *vsauce theme*",
+    "3.0 release or something.",
+    "I added a lot of these for this update",
+    "bob man",
+    "GET *FREE* ROBUX WITH MY LINK IN THE DESCRIPTION 🤑🤑💰"
 ]
 
 function getRandomSplash() {
